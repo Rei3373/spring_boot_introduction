65574de12e468b6b72193861eef87e43f51271a6
Rei3373 <152368897+Rei3373@users.noreply.github.com>
2024-01-03 11:52:27 +0900

    LESSON03

--- src/main/java/com/example/controller/LessonController.java
+++ src/main/java/com/example/controller/LessonController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +21,12 @@ public class LessonController{
 	@ResponseBody
 	public String test(@PathVariable(name = "num") Integer number){
 		return "引数の値: " + number;
+	}
+	
+	@GetMapping("/sample")
+	public String sample(Model model) {
+		String text = "Hello Spring Boot!";
+		model.addAttribute("message", text);
+		return "index";
 	}
 }
